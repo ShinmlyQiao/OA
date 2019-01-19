dd1128fee697fbf356c9db7602734d5cb42e3af9
xuhaixing <haixing1993@163.com>
2019-01-19 23:03:28 +0800

    获取id测试

--- 后端/user-service/src/main/java/com/xingxing/oa/user/service/UserService.java
+++ 后端/user-service/src/main/java/com/xingxing/oa/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.xingxing.oa.user.service;
 
+import com.xingxing.oa.redis.utils.RedisUtils;
 import com.xingxing.oa.user.dao.UserMapper;
 import com.xingxing.oa.user.entity.User;
 import lombok.extern.slf4j.Slf4j;
@@ -16,8 +17,14 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private RedisUtils redisUtils;
+
     @Transactional(rollbackFor = Exception.class)
     public int addUser(User user){
+        Long domainId = redisUtils.getDomainId("user-service::user::id");
+        user.setId(domainId);
+        user.setTenantId(1L);
         Objects.requireNonNull(user.getTenantId(),"租户id不能为空");
         log.info("插入用户:{}", user);
         return userMapper.insert(user);
